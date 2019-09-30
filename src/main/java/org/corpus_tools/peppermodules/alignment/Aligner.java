6d243b797e79d7ef7af8efcc22978773f600fafb
Thomas Krause <thomaskrause@posteo.de>
2019-09-30 14:47:11 +0200

    Do not attempt to add null source/target for pointing relation and output error instead

--- src/main/java/org/corpus_tools/peppermodules/alignment/Aligner.java
+++ src/main/java/org/corpus_tools/peppermodules/alignment/Aligner.java
@@ -39,8 +39,8 @@ import org.osgi.service.component.annotations.Component;
  * Therefore it just prints out some information about a corpus like the number
  * of nodes, edges and for instance annotation frequencies. <br/>
  * This class can be used as a template for an own implementation of a
- * {@link PepperManipulator} Take a look at the TODO's and adapt the code.
- * If this is the first time, you are implementing a Pepper module, we strongly
+ * {@link PepperManipulator} Take a look at the TODO's and adapt the code. If
+ * this is the first time, you are implementing a Pepper module, we strongly
  * recommend, to take a look into the 'Developer's Guide for Pepper modules',
  * you will find on
  * <a href="http://corpus-tools.org/pepper/">http://corpus-tools.org/pepper</a>.
@@ -54,10 +54,9 @@ public class Aligner extends PepperManipulatorImpl {
 	// ===================================================
 	/**
 	 * <strong>OVERRIDE THIS METHOD FOR CUSTOMIZATION</strong> <br/>
-	 * A constructor for your module. Set the coordinates, with which your
-	 * module shall be registered. The coordinates (modules name, version and
-	 * supported formats) are a kind of a fingerprint, which should make your
-	 * module unique.
+	 * A constructor for your module. Set the coordinates, with which your module
+	 * shall be registered. The coordinates (modules name, version and supported
+	 * formats) are a kind of a fingerprint, which should make your module unique.
 	 */
 	public Aligner() {
 		super();
@@ -68,7 +67,7 @@ public class Aligner extends PepperManipulatorImpl {
 		setDesc("This module aligns tokens with pointing relations that carry the same annotation value for two specified annotation names.");
 		setProperties(new AlignerProperties());
 	}
-	
+
 	public PepperMapper createPepperMapper(Identifier Identifier) {
 		AlignmentMapper mapper = new AlignmentMapper();
 		return (mapper);
@@ -76,11 +75,15 @@ public class Aligner extends PepperManipulatorImpl {
 
 	public class AlignmentMapper extends PepperMapperImpl {
 		private static final String ERR_MSG_NO_SUCH_TEXT = "No such text: ";
-		private static final String ERR_MSG_LABEL_ANNO_ERR = "Cannot even partially assign edge labels. No such annotation: {}. Check the provided annotation name or do not set property '" + AlignerProperties.PROP_ANNO_QNAME_ALIGN_LABEL + "'.";				
-		
-		/** The name used as type for the pointing relations as well as relation layer name. */
+		private static final String ERR_MSG_LABEL_ANNO_ERR = "Cannot even partially assign edge labels. No such annotation: {}. Check the provided annotation name or do not set property '"
+				+ AlignerProperties.PROP_ANNO_QNAME_ALIGN_LABEL + "'.";
+
+		/**
+		 * The name used as type for the pointing relations as well as relation layer
+		 * name.
+		 */
 		private String alignmentName = null;
-		
+
 		@Override
 		public DOCUMENT_STATUS mapSDocument() {
 			String sourceTextName = getAlignerProperties().getSourceTextName();
@@ -91,31 +94,31 @@ public class Aligner extends PepperManipulatorImpl {
 			align(getId2TokenMap(sourceTextName, sourceAnnoQName), getId2TokenMap(targetTextName, targetAnnoQName));
 			return (DOCUMENT_STATUS.COMPLETED);
 		}
-		
+
 		private AlignerProperties getAlignerProperties() {
 			return ((AlignerProperties) getProperties());
 		}
-		
+
 		private List<SToken> getTokensByDS(STextualDS ds) {
 			DataSourceSequence<Number> dss = new DataSourceSequence<Number>(ds, ds.getStart(), ds.getEnd());
 			return getDocument().getDocumentGraph().getTokensBySequence(dss);
 		}
-		
+
 		private STextualDS getDSbyName(String dsName) {
-			try { 
-				return getDocument().getDocumentGraph().getTextualDSs().stream().filter((STextualDS tds) -> dsName.equals(tds.getName())).findFirst().get();
-			} 
-			catch (NoSuchElementException e) {
+			try {
+				return getDocument().getDocumentGraph().getTextualDSs().stream()
+						.filter((STextualDS tds) -> dsName.equals(tds.getName())).findFirst().get();
+			} catch (NoSuchElementException e) {
 				throw new PepperModuleDataException(this, ERR_MSG_NO_SUCH_TEXT + dsName, e);
 			}
 		}
-		
+
 		private Map<String, SToken> getId2TokenMap(final String textName, final String annoQName) {
 			SDocumentGraph graph = getDocument().getDocumentGraph();
 			SLayer tokenLayer = SaltFactory.createSLayer();
 			tokenLayer.setName(textName);
 			tokenLayer.setGraph(graph);
-			STextualDS ds = getDSbyName(textName);			
+			STextualDS ds = getDSbyName(textName);
 			Map<SToken, SSpan> token2SpanMap = new HashMap<>();
 			for (SSpan span : graph.getSpans()) {
 				List<SToken> overlappedTokens = graph.getOverlappedTokens(span);
@@ -131,15 +134,17 @@ public class Aligner extends PepperManipulatorImpl {
 					SAnnotation anno = token2SpanMap.get(tok).getAnnotation(annoQName);
 					if (anno != null) {
 						String value = anno.getValue_STEXT();
-						for (String id : value.split(",")) {
-							id2TokenMap.put(id.trim(), tok);
+						if (value != null) {
+							for (String id : value.split(",")) {
+								id2TokenMap.put(id.trim(), tok);
+							}
 						}
 					}
 				}
 			}
 			return id2TokenMap;
 		}
-		
+
 		private void align(Map<String, SToken> sources, Map<String, SToken> targets) {
 			String labelAnnoQName = getAlignerProperties().getAlignmentLabelAnnoQName();
 			SDocumentGraph graph = getDocument().getDocumentGraph();
@@ -150,36 +155,55 @@ public class Aligner extends PepperManipulatorImpl {
 			for (Entry<String, SToken> sourceEntry : sources.entrySet()) {
 				SToken sourceToken = sourceEntry.getValue();
 				SToken targetToken = targets.get(sourceEntry.getKey());
-				SPointingRelation alignRel = (SPointingRelation) graph.createRelation(sourceToken, targetToken, SALT_TYPE.SPOINTING_RELATION, null);
-				alignRel.setType(this.alignmentName);				
-				existingRelations.add(Pair.of(sourceToken.getId(), targetToken.getId()));
-				aLayer.addRelation(alignRel);
+				if (targetToken == null) {
+					String sourceSpan = graph.getText(sourceToken);
+					logger.error("Alignment target \"" + sourceEntry.getKey() + "\" not found for token \"" + sourceSpan
+							+ "\" (" + sourceToken.getId() + ").");
+				} else {
+					SPointingRelation alignRel = (SPointingRelation) graph.createRelation(sourceToken, targetToken,
+							SALT_TYPE.SPOINTING_RELATION, null);
+					alignRel.setType(this.alignmentName);
+					existingRelations.add(Pair.of(sourceToken.getId(), targetToken.getId()));
+					aLayer.addRelation(alignRel);
+				}
 			}
 			for (Entry<String, SToken> targetEntry : targets.entrySet()) {
 				SToken targetToken = targetEntry.getValue();
 				SToken sourceToken = sources.get(targetEntry.getKey());
-				Pair<String, String> nodePair = Pair.of(sourceToken.getId(), targetToken.getId());
-				if (!existingRelations.contains(nodePair)) {
-					SPointingRelation alignRel = (SPointingRelation) graph.createRelation(sourceToken, targetToken, SALT_TYPE.SPOINTING_RELATION, null);					
-					alignRel.setType(this.alignmentName);
-					aLayer.addRelation(alignRel);
+				if (sourceToken == null) {
+					String targetSpan = graph.getText(targetToken);
+					logger.error("Alignment source \"" + targetEntry.getKey() + "\" not found for token \"" + targetSpan
+							+ "\" (" + targetToken.getId() + ").");
+				} else {
+					Pair<String, String> nodePair = Pair.of(sourceToken.getId(), targetToken.getId());
+					if (!existingRelations.contains(nodePair)) {
+						SPointingRelation alignRel = (SPointingRelation) graph.createRelation(sourceToken, targetToken,
+								SALT_TYPE.SPOINTING_RELATION, null);
+						alignRel.setType(this.alignmentName);
+						aLayer.addRelation(alignRel);
+					}
 				}
 			}
-			if (labelAnnoQName != null) {  // annotate alignment edges
+			if (labelAnnoQName != null) { // annotate alignment edges
 				boolean isOnSpan = graph.getSpans().stream().anyMatch((SSpan s) -> s.containsLabel(labelAnnoQName));
 				if (!isOnSpan && graph.getTokens().stream().noneMatch((SToken t) -> t.containsLabel(labelAnnoQName))) {
 					throw new PepperModuleDataException(this, String.format(ERR_MSG_LABEL_ANNO_ERR, labelAnnoQName));
-				}
-				else {
+				} else {
 					for (SRelation rel : aLayer.getRelations()) {
 						SAnnotation edgeLabel = null;
 						if (isOnSpan) {
-							Optional<SRelation> optionalValue = ((SToken) rel.getSource()).getInRelations().stream().filter((SRelation r) -> r instanceof SSpanningRelation && r.getSource().containsLabel(labelAnnoQName)).findFirst();
+							Optional<SRelation> optionalValue = ((SToken) rel.getSource()).getInRelations().stream()
+									.filter((SRelation r) -> r instanceof SSpanningRelation
+											&& r.getSource().containsLabel(labelAnnoQName))
+									.findFirst();
 							if (!optionalValue.isPresent()) {
-								optionalValue = ((SToken) rel.getTarget()).getInRelations().stream().filter((SRelation r) -> r instanceof SSpanningRelation && r.getSource().containsLabel(labelAnnoQName)).findFirst();
+								optionalValue = ((SToken) rel.getTarget()).getInRelations().stream()
+										.filter((SRelation r) -> r instanceof SSpanningRelation
+												&& r.getSource().containsLabel(labelAnnoQName))
+										.findFirst();
 							}
 							if (optionalValue.isPresent()) {
-								edgeLabel = ((SSpan) optionalValue.get().getSource()).getAnnotation(labelAnnoQName);								
+								edgeLabel = ((SSpan) optionalValue.get().getSource()).getAnnotation(labelAnnoQName);
 							}
 						} else {
 							edgeLabel = ((SToken) rel.getSource()).getAnnotation(labelAnnoQName);
@@ -190,8 +214,8 @@ public class Aligner extends PepperManipulatorImpl {
 						if (edgeLabel != null) {
 							rel.createAnnotation(null, edgeLabel.getName(), edgeLabel.getValue());
 						}
-					}						
-				}				
+					}
+				}
 			}
 			graph.removeNode(graph.getTimeline());
 			graph.getTimelineRelations().stream().forEach(graph::removeRelation);
